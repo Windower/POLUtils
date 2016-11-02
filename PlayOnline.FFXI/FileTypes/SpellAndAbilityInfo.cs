0aa8e6766ffe2b22469c42a2e4e0a1d53211e18c
funkworkz <funkworkz@users.noreply.github.com>
2016-11-02 16:23:58 -0500

    Update SpellAndAbilityInfo.cs

--- PlayOnline.FFXI/FileTypes/SpellAndAbilityInfo.cs
+++ PlayOnline.FFXI/FileTypes/SpellAndAbilityInfo.cs
@@ -1,18 +1,18 @@
-// Copyright © 2010-2012 Chris Baggett, Tim Van Holder, Nevin Stepan
-// 
+// Copyright Â© 2010-2012 Chris Baggett, Tim Van Holder, Nevin Stepan
+//
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
 // BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
-
+ 
 using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using PlayOnline.Core;
-
+ 
 namespace PlayOnline.FFXI.FileTypes
 {
     public class SpellAndAbilityInfo : FileType
@@ -52,39 +52,46 @@ namespace PlayOnline.FFXI.FileTypes
             {
                 ProgressCallback(I18N.GetText("FTM:LoadingData"), (double)BR.BaseStream.Position / BR.BaseStream.Length);
             }
+ 
+            //string firstFourBytes = Encoding.ASCII.GetString(BR.ReadBytes(4));
+            //{
+            //    // Part 0: Monster?
+            //    if (firstFourBytes != "mon_")
+            //    {
+            //        goto Part1;
+            //    }
+            //    uint SizeInfo = BR.ReadUInt32();
+            //    if (BR.ReadInt64() != 0)
+            //    {
+            //        goto Failed;
+            //    }
+ 
+            //    //uint BlockSize = (SizeInfo & 0xFFFFFF80) >> 3;
+            //    //if ((BlockSize - 0x10) % 0x58 != 0)
+            //    //{
+            //    //    goto Failed;
+            //    //}
+            //    //uint EntryCount = (BlockSize - 0x10) / 0x58;
+            //    //while (EntryCount-- > 0)
+            //    //{
+            //    //    Things.MonsterSpellInfo2 MSI2 = new Things.MonsterSpellInfo2();
+            //    //    if (!MSI2.Read(BR))
+            //    //    {
+            //    //        goto Failed;
+            //    //    }
+            //    //    if (ProgressCallback != null)
+            //    //    {
+            //    //        ProgressCallback(null, (double)BR.BaseStream.Position / BR.BaseStream.Length);
+            //    //    }
+            //    //    TL.Add(MSI2);
+            //    //}
+ 
+               
+            //}
+ 
+            BR.ReadBytes(1568);
+ 
             string firstFourBytes = Encoding.ASCII.GetString(BR.ReadBytes(4));
-            {
-                // Part 0: Monster?
-                if (firstFourBytes != "mon_")
-                {
-                    goto Part1;
-                }
-                uint SizeInfo = BR.ReadUInt32();
-                if (BR.ReadInt64() != 0)
-                {
-                    goto Failed;
-                }
-                uint BlockSize = (SizeInfo & 0xFFFFFF80) >> 3;
-                if ((BlockSize - 0x10) % 0x58 != 0)
-                {
-                    goto Failed;
-                }
-                uint EntryCount = (BlockSize - 0x10) / 0x58;
-                while (EntryCount-- > 0)
-                {
-                    Things.MonsterSpellInfo2 MSI2 = new Things.MonsterSpellInfo2();
-                    if (!MSI2.Read(BR))
-                    {
-                        goto Failed;
-                    }
-                    if (ProgressCallback != null)
-                    {
-                        ProgressCallback(null, (double)BR.BaseStream.Position / BR.BaseStream.Length);
-                    }
-                    TL.Add(MSI2);
-                }
-            }
-            firstFourBytes = Encoding.ASCII.GetString(BR.ReadBytes(4));
             Part1:
             {
                 // Part 1: Spell Info
@@ -98,11 +105,12 @@ namespace PlayOnline.FFXI.FileTypes
                     goto Failed;
                 }
                 uint BlockSize = (SizeInfo & 0xFFFFFF80) >> 3;
-                if ((BlockSize - 0x10) % 0x58 != 0)
+                if ((BlockSize - 0x10) % 0x64 != 0)
                 {
                     goto Failed;
                 }
-                uint EntryCount = (BlockSize - 0x10) / 0x58;
+                //88 -> 100
+                uint EntryCount = (BlockSize - 0x10) / 0x64;
                 while (EntryCount-- > 0)
                 {
                     Things.SpellInfo2 SI2 = new Things.SpellInfo2();
